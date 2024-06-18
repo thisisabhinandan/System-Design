c0459fd9c621e110adf9863ef8faa7540d7e52d1
thisisabhinandan <abhinandanr@icloud.com>
2024-06-19 00:29:17 +0530

    class vehicle

--- ParkingLot/src/Vehicle.java
+++ ParkingLot/src/Vehicle.java
@@ -1,5 +1,23 @@
 public class Vehicle {
-    private String VechicleNo;
+    private String vechicleNo;
     private VehicleSize vechicleSize;
-
+    public Vehicle(String vechicleNo, VehicleSize vehicleSize )
+    {
+        vechicleNo=this.vechicleNo;
+        vehicleSize=this.vechicleSize;
+    }
+    public VehicleSize getVechicleSize() {
+        return vechicleSize;
+    }
+    public String getVechicleNo()
+    {
+        return vechicleNo;
+    }
+    public void setVechicleNo(String vechicleNo)
+    {
+        vechicleNo=this.vechicleNo;
+    }
+    public void setVechicleSize(VehicleSize vechicleSize) {
+        this.vechicleSize = vechicleSize;
+    }
 }
